b9ede352147ac6cf71b474c71b5f604325e8e2c2
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-21 16:15:55 +0530

    Generic Views

--- shashank/urls.py
+++ shashank/urls.py
@@ -8,14 +8,10 @@ app_name = 'shashank'
 urlpatterns = [
 
     # /shashank/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
 
     # /shashank/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
-
-
-    # /shashank/<album_id>/favorite
-    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
 ]
