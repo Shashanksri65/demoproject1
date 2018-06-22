b2105caf71333a3fc0678d455c2015702e490c30
Shashank Srivastava <shashanksri65@gmail.com>
2018-06-22 09:15:17 +0530

    Upload Files

--- demoproject1/urls.py
+++ demoproject1/urls.py
@@ -7,3 +7,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^shashank/', include('shashank.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
